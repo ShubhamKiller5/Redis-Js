882f49c9371188a29a706c971d9cd9390d3533f2
Shubham Pathak <shubhampathak1@Shubhams-MacBook-Pro.local>
2024-09-03 16:31:53 +0530

    debug:true

--- app/main.js
+++ app/main.js
@@ -3,14 +3,14 @@ const net = require('node:net');
 const server = net.createServer((connection) => {
      // Handle connection
     //  console.log("client connected");
-     connection.on("end", () => {
-        //   console.log("Client disconnected");
-     });
-     connection.write('+PONG\r\n');
-     connection.pipe(connection);
+    //  connection.on("end", () => {
+    //     //   console.log("Client disconnected");
+    //  });
+    connection.write('+PONG\r\n');
+    //  connection.pipe(connection);
 });
-server.on("error", (error) => {
-     throw error;
-});
+// server.on("error", (error) => {
+//      throw error;
+// });
 server.listen(6379, "127.0.0.1");
 
