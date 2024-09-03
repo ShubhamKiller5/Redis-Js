5faeadf41ada671f99a87d15fad8150cc0c6d3d2
Shubham Pathak <shubhampathak1@Shubhams-MacBook-Pro.local>
2024-09-03 16:25:13 +0530

    Add server connection handling and logging

--- app/main.js
+++ app/main.js
@@ -1,11 +1,18 @@
-const net = require("net");
+const net = require('node:net');
 
-// You can use print statements as follows for debugging, they'll be visible when running tests.
-console.log("Logs from your program will appear here!");
-
-// Uncomment this block to pass the first stage
 const server = net.createServer((connection) => {
-  // Handle connection
+     // Handle connection
+     console.log("client connected");
+     connection.on("end", () => {
+          console.log("Client disconnected");
+     });
+     connection.write('+PONG\r\n');
+     connection.pipe(connection);
+});
+server.on("error", (error) => {
+     throw error;
+});
+server.listen(8124, "127.0.0.1", () => {
+    console.log("Server is listening on port 8124");
 });
 
-server.listen(6379, "127.0.0.1");
