a7967fec50b51adf563e4bc1f14e73c017d8e19a
Shubham Pathak <shubhampathak1@Shubhams-MacBook-Pro.local>
2024-09-03 16:56:00 +0530

    handle multiple pings with listening to data

--- app/main.js
+++ app/main.js
@@ -3,9 +3,11 @@ const net = require('node:net');
 const server = net.createServer((connection) => {
      console.log("client connected");
      connection.on("end", () => {
-        //   console.log("Client disconnected");
+          console.log("Client disconnected");
      });
-    connection.write('+PONG\r\n');
+     connection.on('data',()=>{
+         connection.write('+PONG\r\n');
+     })
     //  connection.pipe(connection);
 });
 server.on("error", (error) => {
