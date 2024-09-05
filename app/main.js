836def8fd1cb55512af43d8c6201d9a3688a47b5
Shubham Pathak <shubhampathak1@Shubhams-MacBook-Pro.local>
2024-09-05 17:42:48 +0530

    codecrafters submit [skip ci]

--- app/main.js
+++ app/main.js
@@ -1,5 +1,5 @@
 const net = require("node:net");
-
+const map = {};
 const server = net.createServer((connection) => {
      console.log("client connected");
      connection.on("end", () => {
@@ -24,29 +24,29 @@ server.on("error", (error) => {
 });
 
 // *2\r\n$4\r\nECHO\r\n$3\r\nhey\r\n
-const redisParser = (str='') => {
+const redisParser = (str = '') => {
      const parsedObject = {
-          command: "",
+          command: '',
           commandArg: [],
      };
      const strArray = str.split("\r\n");
-     // const strArray = ["*2","ECHO","banana"]
-     console.log("array", JSON.stringify(strArray));
+     // const strArray = ['*2', 'get', 'fruit'];
+     console.log('array', JSON.stringify(strArray));
      for (const k in strArray) {
           const element = strArray[k];
-          if (element[0] == "$") strArray.splice(k, 1);
+          if (element[0] == '$') strArray.splice(k, 1);
      }
-     console.log("array2", JSON.stringify(strArray));
+     console.log('array2', JSON.stringify(strArray));
      const totalArgs = strArray[0][1];
-     parsedObject["command"] = strArray[1];
-     if (totalArgs == "*1") {
-          parsedObject["commandArg"] = null;
+     parsedObject['command'] = strArray[1];
+     if (totalArgs == '*1') {
+          parsedObject['commandArg'] = null;
           return parsedObject;
      }
      for (let i = 2; i < strArray.length; i++) {
-          console.log("strlen",strArray.length,i);
-          parsedObject["commandArg"] = [
-               ...parsedObject["commandArg"],
+          console.log('strlen', strArray.length, i);
+          parsedObject['commandArg'] = [
+               ...parsedObject['commandArg'],
                strArray[i],
           ];
      }
@@ -54,18 +54,44 @@ const redisParser = (str='') => {
 };
 
 const redisResponse = (command, commandArg) => {
-    console.log(command,commandArg)
-     if (!command || command == undefined) return "-ERR unknown command\r\n";
-     if (command.toLowerCase() == "echo") {
-          const result = commandArg.join(" ");
-          return `$${result.length}\r\n${result}\r\n`;
-     } else if (command.toLowerCase() == "ping") {
-          return "+PONG\r\n";
+     console.log(command, commandArg);
+     if (!command || command == undefined) return '-ERR unknown command\r\n';
+     command = command.toLowerCase();
+     if (command == 'echo') {
+          const result = commandArg.join(' ');
+          return respPattern(result);
+     } else if (command == 'ping') {
+          return '+PONG\r\n';
+     } else if (command == 'set') {
+          const val = commandArg[1];
+          const key = commandArg[0];
+          map[key] = val;
+          console.log(map[key])
+          return `+OK\r\n`;
+     } else if (command == 'get') {
+          const val = map[commandArg[0]];
+          if(val) return respPattern(val);
+          else return respPattern(-1);
      } else {
-          return "-ERR unknown command\r\n";
+          return '-ERR unknown command\r\n';
      }
 };
+
+const respPattern = (val) => {
+     const type = typeof val;
+     let ans = null;
+     switch(type)
+     {
+          case 'string':
+               ans = `$${val.length}\r\n${val}\r\n`;
+               break;
+          case 'number':
+               ans = `$${val}\r\n`;
+               break;
+     }
+     return ans;
+};
 // const res = redisParser();
-// console.log(res)
-// console.log(redisResponse(res.command,res.commandArg));
+// console.log(res);
+// console.log(redisResponse(res.command, res.commandArg));
 server.listen(6379, "127.0.0.1");
