ecfda9f0a2e5f26d551e1a36ff582d209862801a
Shubham Pathak <shubhampathak1@Shubhams-MacBook-Pro.local>
2024-10-11 22:34:47 +0530

    codecrafters submit [skip ci]

--- app/app.js
+++ app/app.js
@@ -17,19 +17,22 @@ if (config.get('isReplica')) {
      //Connect and recieve ping from replica
      await masterRedis.connect();
 
-    console.log('Sending PING');
-    const pingResponse = await masterRedis.sendPing();
-    console.log('PING response:', pingResponse);
+     console.log('Sending PING');
+     const pingResponse = await masterRedis.sendPing();
+     console.log('PING response:', pingResponse);
 
-    console.log('Sending REPLCONF listening-port');
-    const portResponse = await masterRedis.sendListeningPort();
-    console.log('REPLCONF listening-port response:', portResponse);
+     console.log('Sending REPLCONF listening-port');
+     const portResponse = await masterRedis.sendListeningPort();
+     console.log('REPLCONF listening-port response:', portResponse);
 
-    console.log('Sending REPLCONF capa psync2');
-    const capaResponse = await masterRedis.sendCapa();
-    console.log('REPLCONF capa psync2 response:', capaResponse);
+     console.log('Sending REPLCONF capa psync2');
+     const capaResponse = await masterRedis.sendCapa();
+     console.log('REPLCONF capa psync2 response:', capaResponse);
 
-    console.log('Handshake completed');
+     console.log('Sending psync command');
+     const psyncResp = await masterRedis.sendPsync();
+     console.log('Psync response:', psyncResp);
+     console.log('Handshake completed');
 }
 
 const server = net.createServer((connection) => {
--- app/replica.js
+++ app/replica.js
@@ -68,4 +68,10 @@ export class RedisMaster {
       '*3\r\n$8\r\nREPLCONF\r\n$4\r\ncapa\r\n$6\r\npsync2\r\n'
     );
   }
+
+  sendPsync(){
+    return this.sendCommand(
+        '*3\r\n$5\r\nPSYNC\r\n$1\r\n?\r\n$2\r\n-1\r\n'
+    );
+  }
 }
